08de8c336a973d1248eac49ecca29461b82fc068
Sebastian Rittau <sebastian.rittau@zfutura.de>
2026-04-28 12:52:27 +0200

    Actually use tomllib for reading metadata

--- lib/ts_utils/metadata.py
+++ lib/ts_utils/metadata.py
@@ -239,12 +239,7 @@ def read_metadata(distribution: str) -> StubMetadata:
     """
     try:
         with metadata_path(distribution).open("rb") as f:
-            # This cast is necessary for pyright to understand that the
-            # variable is a dict with object values. Just using
-            # `data: dict[str, object] = tomllib.load(f)` doesn't work because
-            # pyright still infers TOMLDocument which derives from
-            # dict[Unknown, Unknown].
-            data = cast(dict[str, object], tomlkit.load(f))
+            data: dict[str, object] = tomllib.load(f)
     except FileNotFoundError:
         raise NoSuchStubError(f"Typeshed has no stubs for {distribution!r}!") from None
 
